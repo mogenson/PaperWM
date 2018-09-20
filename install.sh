6e7665664e6d87515138b54a410e8f9983d40539
Michael Mogenson <michael.mogenson@gmail.com>
2018-09-20 10:19:53 -0400

    [mike] Copy extension contents instead of sym linking
    So I can hack on the source without messing up the loaded extension.

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-EXT_DIR=${XDG_DATA_HOME:-$HOME/.local/share}/gnome-shell/extensions
-mkdir -p "$EXT_DIR"
-ln -s "$(realpath .)" "$EXT_DIR"/'paperwm@hedning:matrix.org'
+rsync -avh . ~/'.local/share/gnome-shell/extensions/paperwm@hedning:matrix.org'
